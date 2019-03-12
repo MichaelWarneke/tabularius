d891211f1faf5b18140f4b266d1406142247bc60
Michael Warneke <michael.warneke@outlook.com.au>
2019-03-12 23:48:46 +0800

    Core and router module init

--- apps/frontend/tabu-app/src/app/app.module.ts
+++ apps/frontend/tabu-app/src/app/app.module.ts
@@ -1,15 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CoreModule } from './core/core.module';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
-  ],
+  imports: [CoreModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
