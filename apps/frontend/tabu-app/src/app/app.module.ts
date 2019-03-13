f5ac32ef3583bd757f83e2c9c3f50a0973a89d9a
Michael Warneke <michael.warneke@outlook.com.au>
2019-03-13 10:23:15 +0800

    tabu-app: Reorganise Core logic

--- apps/frontend/tabu-app/src/app/app.module.ts
+++ apps/frontend/tabu-app/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { CoreModule } from './core/core.module';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { TabuAppSidenavModule } from '@tabularius/ui';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [CoreModule, AppRoutingModule, TabuAppSidenavModule],
+  imports: [CoreModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- apps/frontend/tabu-app/src/app/core/core.module.ts
+++ apps/frontend/tabu-app/src/app/core/core.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { MaterialCoreModule } from '@tabularius/shared-core';
+import { AppRoutingModule } from './app-routing.module';
+import { TabuAppSidenavModule } from '@tabularius/ui';
 
 @NgModule({
   declarations: [],
-  imports: [BrowserModule, MaterialCoreModule]
+  imports: [
+    BrowserModule,
+    MaterialCoreModule,
+    AppRoutingModule,
+    TabuAppSidenavModule
+  ],
+  exports: [TabuAppSidenavModule]
 })
 export class CoreModule {}
