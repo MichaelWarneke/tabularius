7b166d08a06e15f7f176940f39315fa0bd20e56d
Michael Warneke <michael.warneke@outlook.com.au>
2019-03-13 22:51:28 +0800

    shared-model: Update

---
+++ libs/shared/models/src/lib/data/base-models/base.ts
@@ -0,0 +1,7 @@
+export interface ITabuBase {
+  id?: number | string | null;
+  created?: Date | null;
+  createdBy?: string | null;
+  modified?: Date | null;
+  modifiedBy?: string | null;
+}
---
+++ libs/shared/models/src/lib/data/index.ts
@@ -0,0 +1,2 @@
+export * from './base-models';
+export { IAccount } from './account';
