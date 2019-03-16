a23d9e3896749c43f81f70af79eebdea696b681a
Michael Warneke <michael.warneke@outlook.com.au>
2019-03-16 21:29:00 +0800

    ngrx: add effects for root

--- apps/frontend/tabu-app/src/app/core/core.module.ts
+++ apps/frontend/tabu-app/src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { TabuAppSidenavModule } from '@tabularius/ui';
 import { SharedWebCoreModule } from '@tabularius/shared/web-core';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 import { reducers, metaReducers } from './core.state';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -19,6 +20,7 @@ import { CustomSerializer } from '@tabularius/shared/services';
     StoreRouterConnectingModule.forRoot({
       serializer: CustomSerializer
     }),
+    EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument({
       name: 'Tabularius',
       logOnly: environment.production
