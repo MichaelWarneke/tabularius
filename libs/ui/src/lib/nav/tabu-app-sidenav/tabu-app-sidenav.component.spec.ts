14d82d077b4650d140857d840036420ede5376ca
Michael Warneke <michael.warneke@outlook.com.au>
2019-03-16 23:56:53 +0800

    tabu-app-sidenav: fixed testing

--- libs/ui/src/lib/nav/tabu-app-sidenav/tabu-app-sidenav.component.spec.ts
+++ libs/ui/src/lib/nav/tabu-app-sidenav/tabu-app-sidenav.component.spec.ts
@@ -1,16 +1,9 @@
-import 'zone.js';
-import 'zone.js/dist/async-test.js';
-import 'zone.js/dist/proxy.js';
-import 'zone.js/dist/sync-test';
-import 'jest-zone-patch';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { TabuAppSidenavComponent } from './tabu-app-sidenav.component';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { MaterialModule } from '@tabularius/shared-libs';
-import { BreakpointObserver } from '@angular/cdk/layout';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SharedUiModule } from '@tabularius/shared/ui';
 
 describe('NavComponent', () => {
   let component: TabuAppSidenavComponent;
@@ -19,13 +12,8 @@ describe('NavComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [TabuAppSidenavComponent],
-      imports: [
-        NoopAnimationsModule,
-        CommonModule,
-        RouterModule,
-        MaterialModule
-      ],
-      providers: [{ provide: BreakpointObserver, useValue: bpSpy }]
+      imports: [NoopAnimationsModule, RouterTestingModule, SharedUiModule],
+      providers: []
     }).compileComponents();
   }));
 
