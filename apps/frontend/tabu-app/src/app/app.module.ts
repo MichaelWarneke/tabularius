5652d795d85b998f6a327f9d084bdd24035c99e7
Michael Warneke <michael.warneke@outlook.com.au>
2019-03-17 13:26:48 +0800

    service-worker: init

--- apps/frontend/tabu-app/src/app/app.module.ts
+++ apps/frontend/tabu-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CoreModule } from './core/core.module';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [CoreModule],
+  imports: [CoreModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent]
 })
