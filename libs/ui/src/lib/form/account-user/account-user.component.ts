0a00258332cb08f56e701ed2dde3d20862f6c079
Michael Warneke <michael.warneke@outlook.com.au>
2019-03-19 00:56:19 +0800

    account-user: fix update User

--- libs/ui/src/lib/form/account-user/account-user.component.ts
+++ libs/ui/src/lib/form/account-user/account-user.component.ts
@@ -3,7 +3,7 @@ import { IUser } from '@tabularius/shared/models';
 import { Validators, FormBuilder } from '@angular/forms';
 
 export class UserModel {
-  uid = [{ value: '' }, Validators.required];
+  uid = [{ value: '', disabled: true }, Validators.required];
   email = ['', Validators.required];
   displayName = [''];
   photoURL = [''];
@@ -15,11 +15,13 @@ export class UserModel {
   styleUrls: ['./account-user.component.scss']
 })
 export class AccountUserComponent implements OnInit {
+  _user: IUser | null = null;
   @Output() save = new EventEmitter<IUser>();
   @Output() login = new EventEmitter<any>();
   @Output() logout = new EventEmitter<any>();
   @Input()
   set user(item: IUser) {
+    this._user = item;
     if (item) {
       this.form.setValue({
         uid: item.uid,
@@ -38,8 +40,11 @@ export class AccountUserComponent implements OnInit {
   ngOnInit() {}
 
   submit() {
-    this.save.emit({
-      ...this.form.value
-    } as IUser);
+    if (this._user) {
+      this.save.emit({
+        ...this.form.value,
+        uid: this._user.uid
+      } as IUser);
+    }
   }
 }
