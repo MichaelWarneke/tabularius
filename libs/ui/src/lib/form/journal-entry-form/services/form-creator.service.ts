bbf9e61cb3924952e4ea902cc1011645a9d2f930
MichaelWarneke <michael.warneke@outlook.com.au>
2019-03-23 01:04:46 +0800

    modules-app-journal: dynamic forms init

--- libs/ui/src/lib/form/journal-entry-form/services/form-creator.service.ts
+++ libs/ui/src/lib/form/journal-entry-form/services/form-creator.service.ts
@@ -3,15 +3,34 @@ import { Injectable } from '@angular/core';
 import {
   DropdownQuestion,
   QuestionBase,
-  TextboxQuestion
+  TextboxQuestion,
+  DatepickerQuestion
 } from '@tabularius/shared/ui';
-
 @Injectable()
 export class QuestionService {
   // TODO: get from a remote source of question metadata
   // TODO: make asynchronous
   getQuestions() {
     const questions: QuestionBase<any>[] = [
+      new DatepickerQuestion({
+        key: 'date',
+        label: 'Date',
+        required: true,
+        order: 1
+      }),
+
+      new DatepickerQuestion({
+        key: 'dateOfTransaction',
+        label: 'Date of Transaction',
+        order: 2
+      }),
+
+      new DatepickerQuestion({
+        key: 'dateOfEstimateTransaction',
+        label: 'Date of estimated Transaction',
+        order: 3
+      }),
+      /*
       new DropdownQuestion({
         key: 'brave',
         label: 'Bravery Rating',
@@ -23,20 +42,29 @@ export class QuestionService {
         ],
         order: 3
       }),
-
+*/
       new TextboxQuestion({
-        key: 'firstName',
-        label: 'First name',
-        value: 'Bombasto',
-        required: true,
-        order: 1
+        key: 'customer',
+        label: 'Customer',
+        order: 4
       }),
 
       new TextboxQuestion({
-        key: 'emailAddress',
-        label: 'Email',
-        type: 'email',
-        order: 2
+        key: 'supplier',
+        label: 'Supplier',
+        order: 5
+      }),
+
+      new TextboxQuestion({
+        key: 'employee',
+        label: 'Employee',
+        order: 6
+      }),
+
+      new TextboxQuestion({
+        key: 'comment',
+        label: 'Comment',
+        order: 7
       })
     ];
 
