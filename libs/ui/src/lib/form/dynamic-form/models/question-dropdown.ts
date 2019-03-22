2ab150079f1eef8a78b7a2a7c0df7e6f2f09d577
MichaelWarneke <michael.warneke@outlook.com.au>
2019-03-22 12:53:15 +0800

    question-models: fix any

--- libs/ui/src/lib/form/dynamic-form/models/question-dropdown.ts
+++ libs/ui/src/lib/form/dynamic-form/models/question-dropdown.ts
@@ -4,7 +4,7 @@ export class DropdownQuestion extends QuestionBase<string> {
   controlType = 'dropdown';
   options: { key: string; value: string }[] = [];
 
-  constructor(options: {} = {}) {
+  constructor(options: any = {}) {
     super(options);
     this.options = options['options'] || [];
   }
--- libs/ui/src/lib/form/dynamic-form/models/question-textbox.ts
+++ libs/ui/src/lib/form/dynamic-form/models/question-textbox.ts
@@ -4,7 +4,7 @@ export class TextboxQuestion extends QuestionBase<string> {
   controlType = 'textbox';
   type: string;
 
-  constructor(options: {} = {}) {
+  constructor(options: any = {}) {
     super(options);
     this.type = options['type'] || '';
   }
