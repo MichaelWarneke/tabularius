c68c8cf62b45e02a0a3015b8133b78a139f10a25
MichaelWarneke <michael.warneke@outlook.com.au>
2019-03-23 01:05:20 +0800

    forms-setup-tax: dynamic forms init

--- libs/ui/src/lib/form/setup-tax-form/services/form-creator.service.ts
+++ libs/ui/src/lib/form/setup-tax-form/services/form-creator.service.ts
@@ -5,38 +5,61 @@ import {
   QuestionBase,
   TextboxQuestion
 } from '@tabularius/shared/ui';
-
 @Injectable()
 export class QuestionService {
   // TODO: get from a remote source of question metadata
   // TODO: make asynchronous
   getQuestions() {
     const questions: QuestionBase<any>[] = [
-      new DropdownQuestion({
-        key: 'brave',
-        label: 'Bravery Rating',
-        options: [
-          { key: 'solid', value: 'Solid' },
-          { key: 'great', value: 'Great' },
-          { key: 'good', value: 'Good' },
-          { key: 'unproven', value: 'Unproven' }
-        ],
-        order: 3
-      }),
-
       new TextboxQuestion({
-        key: 'firstName',
-        label: 'First name',
-        value: 'Bombasto',
+        key: 'taxCode',
+        label: 'Tax Code',
         required: true,
         order: 1
       }),
 
       new TextboxQuestion({
-        key: 'emailAddress',
-        label: 'Email',
-        type: 'email',
+        key: 'taxDescription',
+        label: 'Tax Descriptiom',
+        required: true,
         order: 2
+      }),
+
+      new TextboxQuestion({
+        key: 'taxPercentageRate',
+        label: 'Tax Percentage Rate',
+        required: true,
+        order: 3
+      }),
+
+      new TextboxQuestion({
+        key: 'basGrossAmount',
+        label: 'BAS Gross Amount',
+        order: 4
+      }),
+
+      new TextboxQuestion({
+        key: 'basTaxAmount',
+        label: 'BAS Tax Amount',
+        order: 5
+      }),
+
+      new TextboxQuestion({
+        key: 'paymentSummaryType',
+        label: 'Payment Summary Type',
+        order: 6
+      }),
+
+      new TextboxQuestion({
+        key: 'grossAmountPayment',
+        label: 'Gross Amount Payment',
+        order: 7
+      }),
+
+      new TextboxQuestion({
+        key: 'taxAmountPayment',
+        label: 'Tax Amount Payment',
+        order: 8
       })
     ];
 
