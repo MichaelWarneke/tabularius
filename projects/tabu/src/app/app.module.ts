229dc931037bf7e09456a31557dc127a901f142b
MichaelWarneke <michael.warneke@outlook.com.au>
2019-11-14 23:10:02 +0800

    (generate) component start

--- projects/tabu/src/app/app.module.ts
+++ projects/tabu/src/app/app.module.ts
@@ -11,11 +11,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { StartComponent } from './views/start/start.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabuNavComponent
+    TabuNavComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule,
